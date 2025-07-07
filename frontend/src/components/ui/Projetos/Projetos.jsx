a342cd7e962c24768a5b9900db27431e3016551b
devDaniel28 <113447778+devdaniel28@users.noreply.github.com>
2025-07-07 20:06:35 -0300

    Criando o componente Projetos.jsx e estilizando ele

--- frontend/src/components/ui/Projetos/Projetos.jsx
+++ frontend/src/components/ui/Projetos/Projetos.jsx
@@ -1,10 +1,36 @@
 const styles = require('./Projetos.module.css')
+import Image from 'next/image'
 
-export default function Projetos() {
+export default function Projetos({title, desq, img, tolls}) {
+
+    const variableTitle = title
+    const variableDesq = desq //* desq == Description
+    const variableImage = `${img}`
+    const {
+      html = false,
+      css = false,
+      javascript = false,
+      react = false,
+      next = false,
+      node = false,
+      express = false,
+      firebase = false,
+    } = tolls || {} //Todo: Terminar o componentes Projetos
 
     return (
         <article className={styles.projetos}>
-            
+            <Image
+                src={variableImage}
+                alt='Imagem do projeto'
+                width={0}
+                height={0}
+                className={styles.projectimg}
+            />
+            <h2>{variableTitle}</h2>
+                <p>{variableDesq}</p>
+            <section>
+
+            </section>
         </article>
     )
 }
